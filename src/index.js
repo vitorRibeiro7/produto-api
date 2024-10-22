77f08baaaeb3f7ba4719307d5dded95d66543820
vitorRibeiro7 <vitorhugomelo5@gmail.com>
2024-10-21 23:05:42 -0300

    feat: finish crud operations

--- src/index.js
+++ src/index.js
@@ -11,19 +11,50 @@ const pool = new Pool({
 
 pool.connect((err, client, release) => {
   if (err) {
-    return console.error("Error acquiring client", err.stack);
+    return console.error("Erro ao se conectar com o banco de dados", err.stack);
   }
-  console.log("Connected to the database!");
+  console.log("Conexão com o banco de dados estabelecida");
   release();
 });
 
 app.get("/", (req, res) => {
-  res.send("Tamo online!");
+  return res.status(200).send("Tamo online!");
 });
 
 app.get("/produtos", async (req, res) => {
   const result = await pool.query("SELECT * FROM produtos");
-  res.json(result.rows);
+  return res.status(200).json(result.rows);
+});
+
+app.get("/produtos/:id", async (req, res) => {
+  const { id } = req.params;
+  const result = await pool.query("SELECT * FROM produtos WHERE id = $1", [id]);
+  return res.status(200).json(...result.rows);
+});
+
+app.post("/produtos", async (req, res) => {
+  const { descricao, preco, estoque, data } = req.body;
+  const result = await pool.query(
+    "INSERT INTO produtos (descricao, preco, estoque, data) VALUES ($1, $2, $3, $4) RETURNING *",
+    [descricao, preco, estoque, data]
+  );
+  return res.status(201).json(...result.rows);
+});
+
+app.put("/produtos/:id", async (req, res) => {
+  const { id } = req.params;
+  const { descricao, preco, estoque, data } = req.body;
+  const result = await pool.query(
+    "UPDATE produtos SET descricao = $1, preco = $2, estoque = $3, data = $4 WHERE id = $5 RETURNING *",
+    [descricao, preco, estoque, data, id]
+  );
+  return res.status(200).json(...result.rows);
+});
+
+app.delete("/produtos/:id", async (req, res) => {
+  const { id } = req.params;
+  const result = await pool.query("DELETE FROM produtos WHERE id = $1", [id]);
+  return res.status(204).send();
 });
 
 app.listen(port, () => {
